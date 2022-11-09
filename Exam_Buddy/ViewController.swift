7465881e66cb6f68c0ed6a8a2109fae3a373fb98
LakmaliPalihakkara <200512551@student.georgianc.on.ca>
2022-11-08 19:39:31 -0500

    Initial Commit

---
+++ Exam_Buddy/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Exam_Buddy
+//
+//  Created by Nirosha on 2022-11-08.
+//  Copyright © 2022 Nirosha. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
